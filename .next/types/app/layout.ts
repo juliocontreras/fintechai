acc54ccb3de548e9d9d96c758222cd60b99b3a5a
Julio Contreras <jcontreras3d@gmail.com>
2025-08-10 23:14:50 +0200

    111222

--- .next/types/app/layout.ts
+++ .next/types/app/layout.ts
@@ -1,4 +1,4 @@
-// File: C:\Users\elhac\Documents\GitHub\fintechai\app\layout.tsx
+// File: C:\Users\julio\Documents\GitHub\fintechai\app\layout.tsx
 import * as entry from '../../../app/layout.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
